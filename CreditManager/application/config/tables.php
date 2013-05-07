a56f5e799b06d286c13a05126672fba4e9b533c5
lhaav <lhaav@ut.ee>
2013-05-07 15:18:48 +0300

    Initial upload - CodeIgniter application configuration files

---
+++ CreditManager/application/config/tables.php
@@ -0,0 +1,12 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Database table names
+|--------------------------------------------------------------------------
+|
+*/
+
+$config['flowsessions_table']   = 'invoices'; // Flow sessions
+$config['widgetsessions_table'] = 'sessions'; // Mashup sessions
+$config['users_table']          = 'users';	  // Users
